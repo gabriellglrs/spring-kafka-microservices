3db6924d64009f9388c2c3fcf79316f359be4cd7
Gabriel Lucas Rodrigues Souza <gabriellucas.souza@inss.gov.br>
2025-10-21 10:52:19 -0300

    feat: primeiro commit

---
+++ clientes/src/test/java/com/gabriellucasdev/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellucasdev.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ produtos/src/test/java/com/devgabriellucas/icompras/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgabriellucas.icompras.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
